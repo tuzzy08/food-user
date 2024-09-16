6f252cf386aaa9fce2cbced74b6be4f141610d9f
xavier <tuzzy08@gmail.com>
2024-09-16 19:43:30 +0100

    Fix: fixed bug with fetching data from api in production

--- hooks/useVendorItems.tsx
+++ hooks/useVendorItems.tsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 
 export function useVendorItems(vendor_id: string) {
 	const QUERY_KEY = `${vendor_id}-all-items`;
-	const API_ENDPOINT = `${process.env.EXPO_PUBLIC_API_URL_DEV}/vendors/${vendor_id}/items`;
+	const API_ENDPOINT = `${process.env.EXPO_PUBLIC_API_URL}/vendors/${vendor_id}/items`;
 	return useQuery<ItemFromAPI[]>({
 		queryKey: [QUERY_KEY],
 		queryFn: () => fetch(API_ENDPOINT).then((res) => res.json()),
